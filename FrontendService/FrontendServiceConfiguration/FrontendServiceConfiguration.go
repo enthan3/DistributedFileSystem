acabbe1cf3b5bcfcedbd9f2984e72a5fe938574d
enthan3 <92779122+enthan3@users.noreply.github.com>
2023-11-29 11:40:21 -0500

    Add Basic structure of frontend service layer

--- FrontendService/FrontendServiceConfiguration/FrontendServiceConfiguration.go
+++ FrontendService/FrontendServiceConfiguration/FrontendServiceConfiguration.go
@@ -1,1 +1,27 @@
 package FrontendServiceConfiguration
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type FrontendServiceConfiguration struct {
+	Address   string `json:"Address"`
+	CacheSize int64  `json:"CacheSize"`
+}
+
+func LoadConfiguration(path string) (FrontendServiceConfiguration, error) {
+	var config FrontendServiceConfiguration
+	configFile, err := os.Open(path)
+	if err != nil {
+		return config, err
+	}
+	defer configFile.Close()
+
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
