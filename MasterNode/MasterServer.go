91f449d4438367b75188c5033b25eaa300ef0e6e
hupei <p27hu@uwaterloo.ca>
2023-12-15 11:39:10 -0500

    Update Master for Master and Master Backup

--- MasterNode/MasterServer.go
+++ MasterNode/MasterServer.go
@@ -16,28 +16,55 @@ func StartMasterServer() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	Logger, err := MaterLogService.InitLogger(Config.LogPath + "MasterLog")
-	if err != nil {
-		log.Fatal(err)
-	}
-	m := MasterRPC.MasterRPCServer{MasterServer: &MasterDefinition.MasterServer{CurrentRPCAddress: Config.CurrentRPCAddress,
-		CurrentBackupRPCAddress: Config.MasterBackupRPCAddress, SlaveRPCAddress: Config.SlavesRPCAddress,
-		FileMetadataName: make(map[string]*Metadata.FileMetaData), FileMetadataID: make(map[string]*Metadata.FileMetaData),
-		ChunkSize: 67108864, StoragePath: Config.StoragePath, ReplicationFactor: 3, LoadBalanceIndex: 0, Logger: Logger, IsBackup: false}}
-	err = rpc.Register(&m)
-	if err != nil {
-		log.Fatal(err)
-	}
-	L, err := net.Listen("tcp", m.MasterServer.CurrentRPCAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		conn, err := L.Accept()
+	if !Config.IsBackup {
+		Logger, err := MaterLogService.InitLogger(Config.LogPath + "MasterLog")
 		if err != nil {
 			log.Fatal(err)
 		}
-		go rpc.ServeConn(conn)
+		m := MasterRPC.MasterRPCServer{MasterServer: &MasterDefinition.MasterServer{CurrentRPCAddress: Config.CurrentRPCAddress,
+			CurrentBackupRPCAddress: Config.MasterBackupRPCAddress, SlaveRPCAddress: Config.SlavesRPCAddress,
+			FileMetadataName: make(map[string]*Metadata.FileMetaData), FileMetadataID: make(map[string]*Metadata.FileMetaData),
+			ChunkSize: 67108864, StoragePath: Config.StoragePath, ReplicationFactor: 3, LoadBalanceIndex: 0, Logger: Logger, IsBackup: false}}
+		err = rpc.Register(&m)
+		if err != nil {
+			log.Fatal(err)
+		}
+		L, err := net.Listen("tcp", m.MasterServer.CurrentRPCAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			conn, err := L.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			go rpc.ServeConn(conn)
+		}
+	} else {
+		Logger, err := MaterLogService.InitLogger(Config.LogPath + "MasterLog")
+		if err != nil {
+			log.Fatal(err)
+		}
+		m := MasterRPC.MasterRPCServer{MasterServer: &MasterDefinition.MasterServer{CurrentRPCAddress: Config.CurrentRPCAddress,
+			CurrentBackupRPCAddress: Config.MasterBackupRPCAddress, SlaveRPCAddress: Config.SlavesRPCAddress,
+			FileMetadataName: make(map[string]*Metadata.FileMetaData), FileMetadataID: make(map[string]*Metadata.FileMetaData),
+			ChunkSize: 67108864, StoragePath: Config.StoragePath, ReplicationFactor: 3, LoadBalanceIndex: 0, Logger: Logger, IsBackup: true}}
+		err = rpc.Register(&m)
+		if err != nil {
+			log.Fatal(err)
+		}
+		L, err := net.Listen("tcp", m.MasterServer.CurrentBackupRPCAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			conn, err := L.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			go rpc.ServeConn(conn)
+		}
 	}
 }
